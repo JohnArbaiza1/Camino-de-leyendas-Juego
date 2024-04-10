cae8ce1e82904374d4da3da9684d90f4fef7e43e
JohnArbaiza1 <johnarbaiza07.03@gmail.com>
2024-04-10 01:28:51 -0600

    Se agrega la parte de ganar cuando se pierde al caer al abismo

--- Mover_ficha.py
+++ Mover_ficha.py
@@ -10,17 +10,20 @@ casillas_penalizacion = [5,17,25,31,45]
 tunel_Seguridad = [46,47,48,49]
 
 #Definimos la funcion que se encargara del desplazamiento de las fichas en el tablero de juego
-def mover_fichas(posicion_jugador,paso1,fichas,cantidadFichas, jugador,listaJugadores):
+def mover_fichas(posicion_jugador,paso1,fichas,cantidadFichas, jugador,listaJugadores,jugadores):
     posicion_jugador += paso1
     if posicion_jugador in casillas_seguras:
-        print(Fore.LIGHTGREEN_EX +"Caíste en tierra protegida: Tienes mucha suerte." + Fore.RESET)
+        print(Fore.LIGHTGREEN_EX +"-"*60,"\nCaíste en tierra protegida: Tienes mucha suerte.(▀̿Ĺ̯▀̿ ̿) \n","-"*60 + Fore.RESET)
     elif posicion_jugador in casillas_penalizacion:
-        print(Fore.LIGHTMAGENTA_EX + "Un mago usa un hechizo contra ti: ahora retrocedes dos casillas " + Fore.RESET)
+        print(Fore.LIGHTMAGENTA_EX +"-"*70, "\nUn mago usa un hechizo contra ti: ahora retrocedes dos casillas (⊙_⊙;) \n","-"*70 + Fore.RESET)
         posicion_jugador -= 2
     elif posicion_jugador in tunel_Seguridad:
-        print(Fore.LIGHTGREEN_EX + "Has llegado al bosque sagrado: Estás en túnel de seguridad"+ Fore.RESET)
+        if posicion_jugador == 49: 
+            print(Fore.LIGHTGREEN_EX +"-"*70,"\nHas ganado el juego: Ahora eres una leyenda (⌐■_■)\n","-"*70 + Fore.RESET)
+        else:
+            print(Fore.LIGHTGREEN_EX +"-"*70, "\nHas llegado al bosque sagrado: Estás en túnel de seguridad ヾ(⌐■_■)ノ♪\n","-"*70 + Fore.RESET)
     elif posicion_jugador >= 50:
-        print(Fore.LIGHTGREEN_EX +"Caiste en el abismo:Pierdes una ficha"+ Fore.RESET)
+        print(Fore.LIGHTMAGENTA_EX +"-"*50,"\nCaíste en el abismo de las serpientes (X_X) \n","-"*50 + Fore.RESET)
         posicion_jugador = 0
         fichasRestantes = 0
         for i in range(cantidadFichas):
@@ -29,7 +32,7 @@ def mover_fichas(posicion_jugador,paso1,fichas,cantidadFichas, jugador,listaJuga
             if fichas_jugador_i[jugador]:
                 fichas_jugador_i[jugador] = False
                 fichas[f"ficha{i+1}"] = fichas_jugador_i
-                print("Has perdido una ficha")
+                print("\nHas perdido una ficha (ㆆ_ㆆ)")
                 break
             
             next_index = i + 1
@@ -39,8 +42,7 @@ def mover_fichas(posicion_jugador,paso1,fichas,cantidadFichas, jugador,listaJuga
                     fichas_jugador_next[jugador] = False
                     fichas[f"ficha{next_index}"] = fichas_jugador_next
                     if not fichas_jugador_i[jugador] and not fichas_jugador_next[jugador]:
-                       # print(f"El Jugador {jugador + 1} ha perdido las vidas de una ficha")
-                        print(f"El jugador {jugador + 1} No tiene mas Fichas")
+                        print(f"El jugador {jugador + 1} No tiene mas Fichas ಥ_ಥ")
                         #listaJugadores[jugador] = False
                     break
 
@@ -51,8 +53,26 @@ def mover_fichas(posicion_jugador,paso1,fichas,cantidadFichas, jugador,listaJuga
 
         if fichasRestantes == 0:
             listaJugadores[jugador] = False
-            print(f"Jugador {jugador+1} ha perdido")
-    elif posicion_jugador == 49: 
-        print(Fore.LIGHTGREEN_EX +"Has ganado el juego: Ahora eres una leyenda"+ Fore.RESET)
-            
+            print(f"Jugador {jugador+1} ha perdido ಥ_ಥ")
+        
+        #Definimos una lista vacia
+        list_novatos = []
+        #Definimos un for para llenar la lista con el numero de jugadores de la partida
+        for i in range(jugadores):
+            list_novatos.append(i+1) 
+        #Transformamos las listas de nuestro jugadores a un diccionario
+        dic_jugadores = dict(zip(list_novatos,listaJugadores))
+        print(dic_jugadores)
+        
+        #Verificamos que solo quede un jugador con fichaz
+        jugadores_activos = sum(1 for estado in dic_jugadores.values() if estado)
+        if jugadores_activos == 1:
+            #Obtenemos el indice de la lista del jugador que gano para mostrarlo
+            ganador = listaJugadores.index(True)
+            print(Fore.LIGHTGREEN_EX +"\n","-"*70)
+            print(f"       FELICIDADES JUGADOR {ganador+1} LO HAS LOGRADO")
+            #De ser asi mostramos el siguiente mensaje y salimos de la ejecucion
+            print("Has ganado el juego: Ahora eres una leyenda (⌐■_■)\n","-"*70 + Fore.RESET)
+            exit()
+    
     return posicion_jugador
